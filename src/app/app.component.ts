61863597e8ef04d1a18c0151afdf99baf05377af
VladimirRiz <resin.fhp@gmail.com>
2022-05-27 18:00:14 +0300

    feat: components events binding

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  serverElements: {type: string, name:string, content: string}[] = [{type:'server', name: 'Testerver', content: 'Just a test'}];
+
+}
